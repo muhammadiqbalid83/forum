c8a970cb0397074721f1299dded01682ec5c5ba5
Muhammad Iqbal <muhammadiqbalid83@gmail.com>
2024-02-16 07:09:37 +0700

    halo

---
+++ script.js
@@ -0,0 +1,4 @@
+const forumLatest = "https://forum-proxy.freecodecamp.rocks/latest";
+const forumTopicUrl = "https://forum.freecodecamp.org/t/";
+const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
+const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
